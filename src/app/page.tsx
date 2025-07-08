529d4913f1f542c012f25c1c137e323110b04759
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-07-08 13:03:01 +0530

    03:BetterAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/db/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const signUp = () => {
+    authClient.signUp.email({
+      email,
+      password,
+      name,
+    });
+  };
+
+  const submitHandler = async () => {
+    await signUp();
+    // console.log(response);
+  };
+
   return (
-    <Button className=" flex justify-center bg-black " >
-      Click Me
-    </Button>
+    <form onSubmit={submitHandler}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <input
+        type="name"
+        value={name}
+        placeholder="Name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
