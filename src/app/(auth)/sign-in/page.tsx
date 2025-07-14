3d7cf7e5956bbfd0ce5429cac25e008cc47e33a1
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-07-14 16:50:10 +0530

    Added auth routes

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import SignInView from "@/modules/auth/ui/view/SignInView";
+import React from "react";
+
+const Page = () => {
+  return <SignInView />;
+};
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import SignUpView from "@/modules/auth/ui/view/SignUpView";
+import React from "react";
+
+const Page = () => {
+  return <SignUpView />;
+};
+
+export default Page;
